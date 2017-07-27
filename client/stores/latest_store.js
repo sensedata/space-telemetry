fcfdac9ddd59f61403bed6618fa05ed5fa054b78
Yuri Gadow <yuri@gadow.us>
2017-07-26 18:52:04 -0600

    Move future timestamp workaround to more common ground: store instead of readout.

--- client/stores/latest_store.js
+++ client/stores/latest_store.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import {Store} from "flummox";
 
+const DELAY_ERROR_MARGIN = 60;
+
 class LatestStore extends Store {
 
   constructor(props) {
@@ -10,7 +12,11 @@ class LatestStore extends Store {
   }
 
   update(data) {
-    const latest = _.max(data, "t");
+    // Some Lightstreamer data is timestamped far in the future, presumably a
+    // bug in their processing or NASA's; this replaces future timestamps.
+    const unixNow = Math.round(Date.now() / 1000) + DELAY_ERROR_MARGIN;
+    const possible = _.filter(data, (d) => d.t <= unixNow);
+    const latest = _.max(possible, "t");
     if (!this.state || latest.t > this.state.t) {
       this.setState(latest);
     }
--- client/views/readouts/last_transmission_readout.jsx
+++ client/views/readouts/last_transmission_readout.jsx
@@ -6,14 +6,10 @@ import ListeningView from "../listening_view.js";
 
 class LastTransmissionReadout extends ListeningView {
   render() {
-    // Some Lightstreamer data is timestamped far in the future, presumably a
-    // bug in their processing or NASA's; this replaces future timestamps.
     const data = null;
     let unixLast = 0;
     if (!_.isEmpty(this.state.data)) {
-      const unixNow = Math.round(Date.now() / 1000);
-      const possible = _.filter(this.state.data, (d) => d.t <= unixNow);
-      unixLast = (possible.length > 0) ? _.max(possible, "t").t : unixNow;
+      unixLast = _.max(this.state.data, "t").t;
     }
     let formatted;
     if (unixLast > 0) {
